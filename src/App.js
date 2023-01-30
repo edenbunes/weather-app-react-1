34269316d610095c343fbf64940e8c52772102a7
edenbunes <115397221+edenbunes@users.noreply.github.com>
2023-01-30 12:17:29 +0200

    Added links to GitHub

--- src/App.js
+++ src/App.js
@@ -4,8 +4,19 @@ import Weather from "./Weather";
 
 export default function App() {
   return (
-    <div>
+    <div className="App container">
       <Weather defaultCity="Tel Aviv" />
+      <p>
+        This project was coded by{" "}
+        <a href="https://github.com/edenbunes/weather-app-react-1">
+          Eden Bunes
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/edenbunes/weather-app-react-1">
+          open-sourced
+        </a>{" "}
+        on <a href="https://github.com/edenbunes/weather-app-react-1">GitHub</a>
+      </p>
     </div>
   );
 }
