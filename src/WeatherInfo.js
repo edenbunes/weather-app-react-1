c5573cdb5b7b180b86ab4f5046f36b0e81158153
edenbunes <115397221+edenbunes@users.noreply.github.com>
2023-02-08 17:59:52 +0200

    Made the app more responsive

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -7,28 +7,41 @@ export default function WeatherInfo(props) {
       <div className="top-information">
         <h1>{props.weatherData.cityName}</h1>
         <div className="row today-forecast">
-          <div className="col-3">
+          <div className="col-6 col-lg">
             <img
               className="main-icon"
               src={props.weatherData.icon}
               alt={props.weatherData.description}
             />
           </div>
-          <div className="col-3 m-auto description text-capitalize">
+          <div className="col-6 col-lg m-auto description text-capitalize">
             {props.weatherData.description}
           </div>
-          <div className="col-2  m-auto">
+          <div className="col-12 col-lg m-auto">
             <div className="temperature">
               {props.weatherData.temp} <span className="unit">c°</span>
             </div>
           </div>
-          <div className="col-2  m-auto">
-            <div>Min temp: {props.weatherData.tempMin}°</div>
-            <div>Max temp: {props.weatherData.tempMax}°</div>
+          <div className="col-6 col-lg m-auto p-0">
+            <div>
+              <span className="extra-info">Min temp:</span>{" "}
+              {props.weatherData.tempMin}°
+            </div>
+            <div>
+              {" "}
+              <span className="extra-info">Max temp:</span>{" "}
+              {props.weatherData.tempMax}°
+            </div>
           </div>
-          <div className="col-2  m-auto">
-            <div>Humidity: {props.weatherData.humidity}%</div>
-            <div>Wind: {props.weatherData.wind} km/h</div>
+          <div className="col-6 col-lg m-auto p-0">
+            <div>
+              <span className="extra-info">Humidity:</span>{" "}
+              {props.weatherData.humidity}%
+            </div>
+            <div>
+              <span className="extra-info">Wind:</span> {props.weatherData.wind}{" "}
+              km/h
+            </div>
           </div>
         </div>
       </div>
